3207ae8e8552b9851a27ac7903740d57b923df0a
Bharat Kapila <bharatkapilabk@gmail.com>
2019-11-13 19:28:41 +0530

    feed and web

--- app/src/androidTest/java/com/example/traincrossinglocator/LoginOrCreateTest.java
+++ app/src/androidTest/java/com/example/traincrossinglocator/LoginOrCreateTest.java
@@ -4,19 +4,13 @@ import android.app.Activity;
 import android.app.Instrumentation;
 import android.support.test.espresso.Espresso;
 import android.support.test.rule.ActivityTestRule;
-
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.action.ViewActions.click;
 import android.view.View;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
--- app/src/main/java/com/example/traincrossinglocator/Home.java
+++ app/src/main/java/com/example/traincrossinglocator/Home.java
@@ -2,18 +2,22 @@ package com.example.traincrossinglocator;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -35,13 +39,11 @@ TrainData trainData;
 NavigationView navigation_view;
 ImageView nav;
 DrawerLayout drawer_layout;
-    ActionBarDrawerToggle mDrawerToggle;
-
-    FirebaseDatabase firebaseDatabase=FirebaseDatabase.getInstance();
-    DatabaseReference databaseReference=firebaseDatabase.getReference("Data").child("Status");
-    DatabaseReference databaseReference1=firebaseDatabase.getReference("Data").child("Time");
-
-
+ActionBarDrawerToggle mDrawerToggle;
+LinearLayout shareapp,viewtrains,viewcrossings,iiiii;
+FirebaseDatabase firebaseDatabase=FirebaseDatabase.getInstance();
+DatabaseReference databaseReference=firebaseDatabase.getReference("Data").child("Status");
+DatabaseReference databaseReference1=firebaseDatabase.getReference("Data").child("Time");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,6 +61,10 @@ DrawerLayout drawer_layout;
         nav=findViewById(R.id.nav);
         navigation_view=findViewById(R.id.navigation_view);
         drawer_layout=findViewById(R.id.drawer_layout);
+        shareapp=findViewById(R.id.shareapp);
+        viewcrossings=findViewById(R.id.viewcrossings);
+        viewtrains=findViewById(R.id.viewtrains);
+
 
         calendar=Calendar.getInstance();
         SimpleDateFormat simpleDateFormat=new SimpleDateFormat("HH:mm");
@@ -85,6 +91,7 @@ nav.setOnClickListener(new View.OnClickListener() {
     public void onClick(View view) {
         drawer_layout.closeDrawer(navigation_view);
         drawer_layout.openDrawer(navigation_view);
+
         mDrawerToggle = new ActionBarDrawerToggle(Home.this, drawer_layout, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
             @SuppressLint("NewApi")
             public void onDrawerSlide(View drawerView, float slideOffset) {
@@ -95,10 +102,19 @@ nav.setOnClickListener(new View.OnClickListener() {
                 float scaleFactor = (max - ((max - min) * slideOffset));
                 }
         };
+        /*mNavItemListner=new NavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Toast.makeText(Home.this,"View Trains", Toast.LENGTH_LONG).show();
+                Log.d("sd", "xgfdgd");
+            return true;
+            }
+        };*/
         drawer_layout.addDrawerListener(mDrawerToggle);
     }
 });
-
+        navigation_view.setNavigationItemSelectedListener(this);
             }
 
     @Override
@@ -107,23 +123,36 @@ nav.setOnClickListener(new View.OnClickListener() {
         finish();
     }
 
-
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Log.d("checkkro", "chalpaya");
         int id=item.getItemId();
         if(id==R.id.viewtrains){
             Toast.makeText(Home.this,"View Trains", Toast.LENGTH_LONG).show();
+            Log.d("sd", "xgfdgd");
+            Intent intent=new Intent(Home.this,ViewTrains.class);
+            startActivity(intent);
         }
 else if(id==R.id.viewcrossings){
             Toast.makeText(Home.this,"View Crossings", Toast.LENGTH_LONG).show();
         }
 else if(id==R.id.feedback){
-            Toast.makeText(Home.this,"Feedback", Toast.LENGTH_LONG).show();
+            Intent browserIntent = new Intent("android.intent.action.VIEW", Uri.parse("https://www.google.com"));
+            startActivity(browserIntent);
         }
 else if(id==R.id.shareapp){
-            Toast.makeText(Home.this,"Share App", Toast.LENGTH_LONG).show();
+            try {
+                Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                shareIntent.setType("text/plain");
+                shareIntent.putExtra(Intent.EXTRA_SUBJECT, "My application name");
+                String shareMessage= "\nLet me recommend you this application\n\n";
+                shareMessage = shareMessage + "https://play.google.com/store/apps/details?id=" + BuildConfig.APPLICATION_ID +"\n\n";
+                shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage);
+                startActivity(Intent.createChooser(shareIntent, "choose one"));
+            } catch(Exception e) {
+                //e.toString();
+            }
         }
 return true;
     }
 }
-
